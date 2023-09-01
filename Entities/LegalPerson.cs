b8ca1962eca4fffdbac7be73c55d6f6654d081b0
wanderson648 <testepedroca@gmail.com>
2023-09-01 19:04:44 -0300

    Classes de pessoa jurídica e física criadas

---
+++ Entities/LegalPerson.cs
@@ -0,0 +1,24 @@
+﻿
+
+namespace Teste.Entities
+{
+    class LegalPerson : Person
+    {
+        public int NumberOfEmployees { get; set; }
+
+        public LegalPerson(string name, double anuualIncome, int numberOfEmployees) : base(name, anuualIncome)
+        {
+            NumberOfEmployees = numberOfEmployees;
+        }
+
+        public override double TotalTax()
+        {
+            if (NumberOfEmployees > 10)
+            {
+                return AnuualIncome * 14 / 100;
+            }
+
+            return AnuualIncome * 16 / 100;
+        }
+    }
+}
---
+++ Entities/PhysicalPerson.cs
@@ -0,0 +1,30 @@
+﻿
+
+namespace Teste.Entities
+{
+    class PhysicalPerson : Person
+    {
+
+        public double HealthExpense { get; set; }
+
+        public PhysicalPerson(string name, double anuualIncome, double healthExpense) : base(name, anuualIncome)
+        {
+            HealthExpense = healthExpense;
+        }
+
+        public override double TotalTax()
+        {
+            double tax = 0;
+
+            if (AnuualIncome >= 20_000 && HealthExpense >= 0)
+            {
+                tax = (AnuualIncome * 25 / 100) - (HealthExpense * 50 / 100);
+            }
+            else if (AnuualIncome < 20_000 && HealthExpense >= 0)
+            {
+                tax = (AnuualIncome * 15 / 100) - (HealthExpense * 50 / 100);
+            }
+            return tax;
+        }
+    }
+}
